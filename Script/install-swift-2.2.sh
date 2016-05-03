94daebed19c1069a6eb5a3822bcc5b79a56b043c
Jake Lin <JakeLinAu@gmail.com>
2016-05-03 23:09:17 +1000

    Add `allTests` to `EachTest`

--- Script/install-swift-2.2.sh
+++ Script/install-swift-2.2.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -O https://swift.org/builds/swift-2.2-branch/ubuntu1404/swift-2.2-SNAPSHOT-2016-01-11-a/swift-2.2-SNAPSHOT-2016-01-11-a-ubuntu14.04.tar.gz
 tar zxf swift-2.2-SNAPSHOT-2016-01-11-a-ubuntu14.04.tar.gz
 sudo chown -R vagrant:vagrant swift-*
